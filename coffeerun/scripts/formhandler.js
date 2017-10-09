9b64a9042952b091c5477931b5e4a6210195cf3d
Aishwarya Iyer <aish11893@gmail.com>
2017-10-09 10:41:09 -0700

    Initial

--- coffeerun/scripts/formhandler.js
+++ coffeerun/scripts/formhandler.js
@@ -1,8 +1,7 @@
-(function(window) {
+(function (window) {
   'use strict';
   var App = window.App || {};
   var $ = window.jQuery;
-
 
   function FormHandler(selector) {
     if (!selector) {
@@ -15,24 +14,40 @@
     }
   }
 
-  FormHandler.prototype.addSubmitHandler = function(fn) {
+
+  FormHandler.prototype.addSumbitHandler = function (fn) {
     console.log('Setting submit handler for form');
-    this.$formElement.on('submit', function(event) {
+    this.$formElement.on('submit', function (event) {
       event.preventDefault();
 
       var data = {};
-      $(this).serializeArray().forEach(function(item) {
+      $(this).serializeArray().forEach(function (item) {
         data[item.name] = item.value;
         console.log(item.name + ' is ' + item.value);
       });
       console.log(data);
+
       fn(data);
       this.reset();
       this.elements[0].focus();
     });
+  };
 
+  FormHandler.prototype.addInputHandler = function (fn) {
+    console.log('Setting input handler for form');
+    this.$formElement.on('input', '[name="emailAddress"]', function (event) {
+      var emailAddress = event.target.value;
+      var message = '';
+      if (fn(emailAddress)) {
+        event.target.setCustomValidity('');
+      } else {
+        message = emailAddress + ' is not an authorized email address!';
+        event.target.setCustomValidity(message);
+      }
+    });
   };
 
   App.FormHandler = FormHandler;
   window.App = App;
+
 })(window);
